d84037cbde2ca53600511ed3f3a5b8fabf3305de
CodingMastermind123 <145368068+CodingMastermind123@users.noreply.github.com>
2024-04-23 21:54:30 -0500

    Card.java

--- Card.java
+++ Card.java
@@ -1,29 +1,76 @@
+
 import mayflower.*;
-public class Card
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Collections;
+
+public class Card extends Actor
 {
-    private String rank;
-    private String suit;
+    // instance variables - replace the example below with your own
     private MayflowerImage image;
-
-    /**
-     * Constructor for objects of class Card
-     */
-    public Card(String rank, String suit, Mayflower image)
+    Queue<String> clubs = new LinkedList<String>();
+    Queue<String> diamonds = new LinkedList<String>();
+    Queue<String> spades = new LinkedList<String>();
+    Queue<String> hearts = new LinkedList<String>();
+    Queue<String> selected;
+    public Card()
     {
-        this.rank = rank;
-        this.suit = suit;
-        image = image;
-    }
-    
-    public String getRank() {
-        return rank;
+        // int i = (int)(Math.Random() * 8) + 2;
+        for(int i = 2; i < 10; i++)
+        {
+            clubs.add("Cards/Clubs " + i + ".png");
+        }
+        for(int i = 2; i < 10; i++)
+        {
+            diamonds.add("Cards/Diamonds " + i + ".png");
+        }
+         for(int i = 2; i < 10; i++)
+        {
+            spades.add("Cards/Spades " + i + ".png");
+        }
+        for(int i = 2; i < 10; i++)
+        {
+            hearts.add("Cards/Hearts " + i + ".png");
+        }
+        randomSelect();
+        shuffle(selected);
+        setImage(selected.remove());
     }
 
-    public String getSuit() {
-        return suit;
+    public void act()
+    {
+        
+        
     }
+    public void randomSelect()
+    {
+        int random = (int)(Math.random() * 4) + 1;
+        if(random == 1)
+        {
+            selected = clubs;
+        }
+        else if(random == 2)
+        {
+            selected = diamonds;
+        }
+        else if(random == 3)
+        {
+            selected = spades;
+        }
+        else
+        {
+            selected = hearts;
+        }
+    }
+    public void shuffle(Queue queue)
+    {
+        List<String> list = new LinkedList<>(queue);
 
-    public MayflowerImage getImage() {
-        return image;
+        Collections.shuffle(list);
+
+        queue.clear();
+        queue.addAll(list);
+        
     }
-}
+    }
