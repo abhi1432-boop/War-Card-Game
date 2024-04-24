739ad63e381c9f84febde02fce7e5ddaa2b9d5bb
CodingMastermind123 <145368068+CodingMastermind123@users.noreply.github.com>
2024-04-23 21:55:48 -0500

    GameWorld.java

--- GameWorld.java
+++ GameWorld.java
@@ -2,9 +2,11 @@
 import mayflower.*;
 public class GameWorld extends World
 {
-     public GameWorld()
+     public Card card;
+    public GameWorld()
     {
-       
+       card = new Card();
+       addObject(card, 900, 600);
        setBackground("Solitaire.jpg");
     }
     public void act() {
