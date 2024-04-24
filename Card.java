d28df71c15cf0d4e13eca7b2848bef4cff58a4e7
The Thinker <62044304+ThinkerDesigns@users.noreply.github.com>
2024-04-23 19:49:13 -0500

    x

--- Card.java
+++ Card.java
@@ -1,8 +1,6 @@
-
 import mayflower.*;
 public class Card
 {
-    // instance variables - replace the example below with your own
     private String rank;
     private String suit;
     private MayflowerImage image;
@@ -12,18 +10,11 @@ public class Card
      */
     public Card(String rank, String suit, Mayflower image)
     {
-        // initialise instance variables
         this.rank = rank;
         this.suit = suit;
-        this.image = image;
+        image = image;
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
+    
     public String getRank() {
         return rank;
     }
